a098810f51abb231193ad19065c665465427a658
Louyti <lorensytas@gmail.com>
2020-10-28 18:16:26 +0200

    loading screen added

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,18 +1,18 @@
 document.getElementById("windToggle").addEventListener("click", () => { //toggle wind overlay
-    let co = document.getElementById("windToggle")
-    let c = co.getAttribute("clicked") === "1";
-    co.setAttribute("clicked", c ? "0" : "1");
-    co.style.opacity = c ? "0.5" : "1";
+	let co = document.getElementById("windToggle");
+	let c = co.getAttribute("clicked") === "1";
+	co.setAttribute("clicked", c ? "0" : "1");
+	co.style.opacity = c ? "0.5" : "1";
 });
 document.getElementById("playToggle").addEventListener("click", () => { //toggle simulation playing
-    let co = document.getElementById("playToggle")
-    let c = co.getAttribute("clicked") === "1";
-    co.setAttribute("clicked", c ? "0" : "1");
-    co.style.opacity = c ? "0.5" : "1";
+	let co = document.getElementById("playToggle");
+	let c = co.getAttribute("clicked") === "1";
+	co.setAttribute("clicked", c ? "0" : "1");
+	co.style.opacity = c ? "0.5" : "1";
 });
 document.getElementById("sumToggle").addEventListener("click", () => { //toggle sum overlay
-    let co = document.getElementById("sumToggle")
-    let c = co.getAttribute("clicked") === "1";
-    co.setAttribute("clicked", c ? "0" : "1");
-    co.style.opacity = c ? "0.5" : "1";
+	let co = document.getElementById("sumToggle");
+	let c = co.getAttribute("clicked") === "1";
+	co.setAttribute("clicked", c ? "0" : "1");
+	co.style.opacity = c ? "0.5" : "1";
 });
